071508c491584d282bdbdfb195def22ac7cb992a
yejf <490293266@qq.com>
2020-06-09 15:08:22 +0800

    update

--- routes/login.js
+++ routes/login.js
@@ -4,32 +4,32 @@ const router = express.Router()
 const config = require('../config')
 
 //登录
-router.post('/login', function (req, res) {
-	console.log(req.body, 12)
-	const { USER_NAME, PWD } = config
+router.post('/login', function(req, res) {
+  console.log(req.body, 12)
+  const { USER_NAME, PWD } = config
   const { username, password } = req.body
   if (username !== USER_NAME) {
-  	res.send({
-  		code: 40001,
-  		msg: '用户名错误'
-  	})
-  	return
+    res.send({
+      code: 40001,
+      msg: '用户名错误'
+    })
+    return
   }
 
   if (password !== PWD) {
-  	res.send({
-  		code: 40002,
-  		msg: '密码错误'
-  	})
-  	return 
+    res.send({
+      code: 40002,
+      msg: '密码错误'
+    })
+    return
   }
 
   if (username == USER_NAME && password == PWD) {
-  	res.send({
-  		code: 200,
-  		msg: '登录成功'
-  	})
-  	return
+    res.send({
+      code: 200,
+      msg: '登录成功'
+    })
+    return
   }
 })
 
--- routes/produceWord.js
+++ routes/produceWord.js
@@ -2,68 +2,155 @@ const express = require('express')
 const router = express.Router()
 
 var JSZip = require('jszip')
+var PizZip = require('pizzip')
 var Docxtemplater = require('docxtemplater')
 
 var fs = require('fs')
 var path = require('path')
 
-//生成word
-router.post('/produceword', function (req, res) {
-	const { docxName, docxParams } = req.body
-	const docPath = '../temp/' + docxName + '.docx'
-	const docOutPath = '../output/' + docxName + '.docx'
-	// console.log(docPath, docOutPath)
-	//Load the docx file as a binary
-	try {
+function startZip(pathName, tempName) {
+  const zip = new JSZip()
+  const targetDir = path.resolve(__dirname, pathName)
 
-		var content = fs
-    	.readFileSync(path.resolve(__dirname, docPath), 'binary')
+  // 读取目录中的所有文件及文件夹
+  const files = fs.readdirSync(targetDir)
+  //遍历检测目录中的文件
+  files.forEach(function(fileName) {
+    const fillPath = targetDir + '/' + fileName
+    zip.file(fileName, fs.readFileSync(fillPath)) //压缩目录添加文件
+  })
 
-    var zip = new JSZip(content)
+  zip
+    .generateAsync({
+      //设置压缩格式
+      type: 'nodebuffer', //nodejs用
+      compression: 'DEFLATE', //压缩算法
+      compressionOptions: {
+        //压缩级别
+        level: 9
+      }
+    })
+    .then(function(content) {
+      fs.writeFile(`./output/${tempName}.zip`, content, 'binary', function() {
+        console.log(`生成${tempName}.zip成功，时间 ${new Date()}`)
+      })
 
-    var doc = new Docxtemplater()
-		doc.loadZip(zip)
+      //将打包的内容写入 当前output的 .zip中
+      // fs.writeFileSync(
+      //   path.resolve(__dirname, `/output/${tempName}.zip`),
+      //   content,
+      //   'binary'
+      // )
 
-		doc.setData(docxParams)
+      // saveAs()
+      // see FileSaver.js npm install file-saver
+      // console.log(content, 121212)
+      // FileSaver.saveAs(content, '11.zip')
+    })
+}
 
-		try {
-			// render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-			doc.render()
-		} catch (err) {
-			var e = {
+router.post('/produceword-muti', function(req, res) {
+  const { docxName = [], tempName, docxParams } = req.body
+
+  const docxSize = docxName.length
+  for (let i = 0; i < docxSize; i++) {
+    const name = docxName[i]
+    const docPath = `../temp/${tempName}/${name}.docx`
+    const docOutPath = `../output/${tempName}/${name}.docx`
+    // console.log(docPath, 'for ---------')
+
+    try {
+      const content = fs.readFileSync(
+        path.resolve(__dirname, docPath),
+        'binary'
+      )
+      const zip = new PizZip(content)
+      // console.log(zip, 111)
+
+      const doc = new Docxtemplater()
+      doc.loadZip(zip)
+      doc.setData(docxParams)
+
+      try {
+        doc.render()
+      } catch (err) {
+        const e = {
+          message: error.message,
+          name: error.name,
+          stack: error.stack,
+          properties: error.properties
+        }
+        console.log(JSON.stringify({ error: e }))
+        throw error
+      }
+      const buf = doc.getZip().generate({ type: 'nodebuffer' })
+      fs.writeFileSync(path.resolve(__dirname, docOutPath), buf)
+    } catch (e) {
+      console.log(JSON.stringify(e))
+      res.send({
+        code: -1,
+        msg: '服务器故障'
+      })
+    }
+  }
+
+  // 压缩文件
+  const targetDir = `../output/${tempName}`
+  startZip(targetDir, tempName)
+
+  res.send({
+    code: 200,
+    msg: '提交成功'
+  })
+})
+
+//生成 单个 word API
+router.post('/produceword', function(req, res) {
+  const { docxName, docxParams } = req.body
+  const docPath = '../temp/' + docxName + '.docx'
+  const docOutPath = '../output/' + docxName + '.docx'
+
+  // console.log(docPath, docOutPath)
+  //Load the docx file as a binary
+  try {
+    const content = fs.readFileSync(path.resolve(__dirname, docPath), 'binary')
+
+    const zip = new PizZip(content)
+
+    const doc = new Docxtemplater()
+    doc.loadZip(zip)
+    doc.setData(docxParams)
+
+    try {
+      // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+      doc.render()
+    } catch (err) {
+      const e = {
         message: error.message,
         name: error.name,
         stack: error.stack,
-        properties: error.properties,
-    	}
-    	console.log(JSON.stringify({error: e}))
-    	// The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
-    	throw error
-		}
+        properties: error.properties
+      }
+      console.log(JSON.stringify({ error: e }))
+      // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
+      throw error
+    }
 
-		var buf = doc.getZip()
-             .generate({type: 'nodebuffer'})
-
+    const buf = doc.getZip().generate({ type: 'nodebuffer' })
     // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-		fs.writeFileSync(path.resolve(__dirname, docOutPath), buf)
-	
-		res.send({
-			code: 200,
-			msg: '提交成功'
-		})
+    fs.writeFileSync(path.resolve(__dirname, docOutPath), buf)
 
-	} catch (e) {
-		console.log(JSON.stringify(e))
-		res.send({
-			code: -1,
-			msg: '服务器故障'
-		})
-	}
+    res.send({
+      code: 200,
+      msg: '提交成功'
+    })
+  } catch (e) {
+    console.log(JSON.stringify(e))
+    res.send({
+      code: -1,
+      msg: '服务器故障'
+    })
+  }
 })
 
 module.exports = router
-
-
-
-
-
